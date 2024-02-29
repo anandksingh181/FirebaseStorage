d4f264320ac47b35b3b41b0188428df7ead370ac
anandksingh181 <142203756+anandksingh181@users.noreply.github.com>
2024-02-29 22:55:15 +0530

    VideoUploadStorage

--- app/src/main/java/com/example/firebasestorage/VideoActivity.kt
+++ app/src/main/java/com/example/firebasestorage/VideoActivity.kt
@@ -2,8 +2,10 @@ package com.example.firebasestorage
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.MimeTypeMap
 import android.widget.MediaController
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -12,7 +14,6 @@ import com.example.firebasestorage.databinding.ActivityVideoBinding
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
-import com.squareup.picasso.Picasso
 
 class VideoActivity : AppCompatActivity() {
     lateinit var binding: ActivityVideoBinding
@@ -31,15 +32,28 @@ class VideoActivity : AppCompatActivity() {
     val videoLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
         if(it.resultCode == Activity.RESULT_OK){
             if (it.data!=null){
-              binding.videoBtn.isVisible = false
-                binding.videoView.isVisible = true
-                val mediaController = MediaController(this)
-                mediaController.setAnchorView(binding.videoView)
+                val ref = Firebase.storage.reference.child("Video/"+System.currentTimeMillis()+"."+getFileType(it.data!!.data))
+                ref.putFile(it.data!!.data!!).addOnSuccessListener {
+                    ref.downloadUrl.addOnSuccessListener {
 
-                binding.videoView.setVideoURI(it.data!!.data)
-                binding.videoView.setMediaController(mediaController)
-                binding.videoView.start()
+                        Firebase.database.reference.child("Video").push().setValue(it.toString())
+                        Toast.makeText(this, "Video Uploaded", Toast.LENGTH_SHORT).show()
+                        binding.videoBtn.isVisible = false
+                        binding.videoView.isVisible = true
+                        val mediaController = MediaController(this)
+                        mediaController.setAnchorView(binding.videoView)
+
+                        binding.videoView.setVideoURI(it)
+                        binding.videoView.setMediaController(mediaController)
+                        binding.videoView.start()
+                    }
+                }
+
             }
         }
+    }
+    private fun getFileType(data: Uri?): String? {
+        val mimeType = MimeTypeMap.getSingleton()
+        return mimeType.getMimeTypeFromExtension(contentResolver.getType(data!!))
     }
 }
